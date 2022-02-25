b898f67e01a53843bf3c90ae9ca4f7f85de78f6f
Prk <71644464+BiliPrk@users.noreply.github.com>
2022-02-25 22:25:48 +0800

    Create Remove.sh

---
+++ Tencent/Cloud/Remove.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Name: Remove Tencent Cloud YunJing...
+# Author: Prk
+# WebSite: https://www.cprk.cc/
+# Date: 2022-02-25
+# Location: Huazhou Haizhu Guangzhou Guangdong, People's Republic of China
+
+# Execute the Official Uninstaller #
+/usr/local/qcloud/stargate/admin/uninstall.sh
+/usr/local/qcloud/YunJing/uninst.sh
+/usr/local/qcloud/monitor/barad/admin/uninstall.sh
